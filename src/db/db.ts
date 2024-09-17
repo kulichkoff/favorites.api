ba493760392e043992770033d39a7296765208cf
Daniel Kulichkov <danchick03@gmail.com>
2024-09-17 19:09:58 +0300

    feat: add simple db

---
+++ src/db/db.ts
@@ -0,0 +1,33 @@
+export interface FavoritesShareModel {
+  id: string;
+  favorites: string[];
+}
+
+export class ShareDb {
+  private _storage: FavoritesShareModel[] = [];
+
+  private static _instance?: ShareDb;
+
+  public static getInstance(): ShareDb {
+    if (!this._instance) {
+      return new ShareDb();
+    } else {
+      return this._instance;
+    }
+  }
+
+  public create(favorites: string[]): FavoritesShareModel {
+    const newItem = {
+      id: 'some id',
+      favorites,
+    };
+
+    this._storage.push(newItem);
+
+    return newItem;
+  }
+
+  public find(id: string): FavoritesShareModel | null {
+    return this._storage.find((shareitem) => shareitem.id === id) ?? null;
+  }
+}
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,13 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { CreateShareLinkDto } from './dto/create-share-link.dto';
+import { ShareDb } from 'src/db/db';
 
 @Injectable()
 export class FavoritesService {
+  private readonly db = new ShareDb();
+
   createShare(createShareLinkDto: CreateShareLinkDto) {
-    return 'This action adds a new share';
+    const result = this.db.create(createShareLinkDto.favorites);
+    Logger.log(`Created new favorites share ${JSON.stringify(result)}`);
+    return result;
   }
 
   findShare(id: string) {
-    return `This action returns a #${id} share`;
+    const found = this.db.find(id);
+
+    if (found) {
+      return found;
+    } else {
+      throw new HttpException(
+        'Could not find share with id ' + id,
+        HttpStatus.NOT_FOUND,
+      );
+    }
   }
 }
